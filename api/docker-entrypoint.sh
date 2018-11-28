8d7153f8f6db41319599b7708f6f995111c78941
Yohan Giarelli <yohan@un-zero-un.fr>
2018-11-28 13:45:26 +0100

    Initial working commit

---
+++ api/docker-entrypoint.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+set -e
+
+# first arg is `-f` or `--some-option`
+if [ "${1#-}" != "$1" ]; then
+    set -- php-fpm "$@"
+fi
+
+if [ "$1" = 'php-fpm' ] || [ "$1" = 'php' ] || [ "$1" = 'bin/console' ]; then
+    mkdir -p var/cache var/log
+    setfacl -R -m u:www-data:rwX -m u:"$(whoami)":rwX var
+    setfacl -dR -m u:www-data:rwX -m u:"$(whoami)":rwX var
+
+    if [ "$APP_ENV" != 'prod' ]; then
+        ln -sf ${PHP_INI_DIR}/php.ini-development ${PHP_INI_DIR}/php.ini
+        composer install --prefer-dist --no-progress --no-suggest --no-interaction
+    else
+        ln -sf ${PHP_INI_DIR}/php.ini-production ${PHP_INI_DIR}/php.ini
+    fi
+
+#    >&2 echo "Waiting for Postgres to be ready..."
+#    until pg_isready --timeout=0 --dbname="${DATABASE_URL}"; do
+#        sleep 1
+#    done
+fi
+
+exec docker-php-entrypoint "$@"
