2bc475bc8993d1c9ba13f0905f5458c7c06c17af
Yohan Giarelli <yohan@un-zero-un.fr>
2018-12-02 12:51:46 +0100

    Making tries with service worker

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -62,7 +62,5 @@ renderMethod(
 );
 
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+serviceWorker.register();
