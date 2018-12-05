c97905aad1a9205fc27e922376b3e426f18b777f
Yohan Giarelli <yohan@un-zero-un.fr>
2018-12-05 17:26:07 +0100

    Added a No-SSR mode

--- app/server.js
+++ app/server.js
@@ -13,17 +13,35 @@ app.use('/sw.js', express.static(path.resolve(__dirname, 'build/sw.js')));
 app.use('/favicon.ico', express.static(path.resolve(__dirname, 'build/favicon.ico')));
 app.use('/app.webmanifest', express.static(path.resolve(__dirname, 'build/app.webmanifest')));
 
+function renderHtml(content, css, state = null, apolloState = null, helmet = null) {
+    return indexTemplate
+        .replace(/<div id="root"><\/div>/g, `<div id="root">${content}</div>`)
+        .replace(/<style id="jss-server-side"><\/style>/g, `<style id="jss-server-side">${css}</style>`)
+        .replace(/<\/head>/g, `
+                <script>
+                    window.__REDUX_STATE = ${JSON.stringify(state)};
+                    window.__APOLLO_STATE=${JSON.stringify(apolloState)};
+                </script>
+                ${helmet ? helmet.title.toString() : ''}
+                ${helmet ? helmet.meta.toString() : ''}
+                ${helmet ? helmet.link.toString() : ''}
+            </head>
+        `);
+}
+
 app.get('/*', (req, res) => {
     console.log(`Displaying ${req.url}`);
 
+    if ('false' === process.env.SSR_ENABLED) {
+        res.send(renderHtml('', ''));
+
+        return;
+    }
+
+
     ssr(req.url)
         .then(({ state, content, apolloState, helmet, css }) => {
-            res.send(
-                indexTemplate
-                    .replace(/<div id="root"><\/div>/g, `<div id="root">${content}</div>`)
-                    .replace(/<style id="jss-server-side"><\/style>/g, `<style id="jss-server-side">${css}</style>`)
-                    .replace(/<\/head>/g, `<script>window.__REDUX_STATE = ${JSON.stringify(state)};window.__APOLLO_STATE=${JSON.stringify(apolloState)};</script>${helmet.title.toString()}${helmet.meta.toString()}${helmet.link.toString()}</head>`)
-            );
+            res.send(renderHtml(content, css, state, apolloState, helmet));
         })
         .catch((e) => {
             console.error(e);
@@ -33,6 +51,6 @@ app.get('/*', (req, res) => {
 });
 
 app.disable('x-powered-by');
-app.listen(process.env.PORT || 3000, function () {
-    console.log(`app Server is now running on http://localhost:${process.env.PORT || 3000}`);
+app.listen(process.env.SSR_PORT || 3001, function () {
+    console.log(`app Server is now running on http://localhost:${process.env.SSR_PORT || 3001}`);
 });
