b252e87bd8311f3a4089a93ad2cdc3699b29696f
Yohan Giarelli <yohan@un-zero-un.fr>
2018-12-09 12:57:24 +0000

    Made errors bubble.

--- app/src/quote/containers/CreateQuote.tsx
+++ app/src/quote/containers/CreateQuote.tsx
@@ -1,15 +1,41 @@
+import {Mutation} from "react-apollo";
+import {GraphQLError} from "graphql";
+import {SubmissionError} from "redux-form";
 import React, {Component} from "react";
+
 import QuoteForm from "../forms/QuoteForm";
 import {Quote} from "../types/Quote";
-import {Mutation} from "react-apollo";
 
 export default class CreateQuote extends Component<{}> {
     render() {
         return (
             <Mutation mutation={require('../mutations/createQuote.graphql')}>
-                {(createTodo) => (
-                    <QuoteForm onSubmit={(quote: Quote) => createTodo({variables: quote})} />
-                )}
+                {
+                    createTodo => {
+                        return <QuoteForm onSubmit={async (quote: Quote) => {
+                            try {
+                                const res = await createTodo({variables: quote});
+
+                                console.log('THE RESP');
+                                console.log(res);
+                            } catch (e) {
+                                const errors = e.graphQLErrors.reduce(
+                                    (memo: { [path: string]: string }, error: GraphQLError) => {
+                                        const message = error.message.split(/: /);
+
+                                        return ({
+                                            ...memo,
+                                            [message[0]]: [message[1]],
+                                        })
+                                    },
+                                    {}
+                                );
+
+                                throw new SubmissionError<Quote>(errors);
+                            }
+                        }}/>
+                    }
+                }
             </Mutation>
         );
     }
--- app/src/quote/forms/QuoteForm.tsx
+++ app/src/quote/forms/QuoteForm.tsx
@@ -1,29 +1,40 @@
 import React from 'react';
 import {reduxForm, InjectedFormProps, Field} from "redux-form";
+import {connect} from "react-redux";
+
 import {Quote} from "../types/Quote";
 
+type Errors = { [path: string]: string };
 type Props = {
-
+    errors?: Errors
 }
 
 type FormProps = InjectedFormProps<Quote, Props> & Props;
 
-const QuoteForm = ({handleSubmit}: FormProps) => (
-    <form onSubmit={handleSubmit}>
-        <div>
-            <label htmlFor="author">Author</label>
-            <Field name="author" component="input" type="text" />
-        </div>
-        <div>
-            <label htmlFor="author">Title</label>
-            <Field name="title" component="input" type="text" />
-        </div>
-        <div>
-            <label htmlFor="author">Text</label>
-            <Field name="text" component="input" type="text" />
-        </div>
-        <button type="submit">Submit</button>
-    </form>
-);
+const QuoteForm = ({handleSubmit, pristine, submitting, errors}: FormProps) => {
+    console.log(pristine, submitting, errors);
 
-export default reduxForm<Quote, Props>({form: 'quote'})(QuoteForm);
+    return (
+        <form onSubmit={handleSubmit}>
+            <div>
+                <label htmlFor="author">Author</label>
+                <Field name="author" component="input" type="text" disabled={submitting} />
+            </div>
+            <div>
+                <label htmlFor="author">Title</label>
+                <Field name="title" component="input" type="text" disabled={submitting} />
+            </div>
+            <div>
+                <label htmlFor="author">Text</label>
+                <Field name="text" component="input" type="text" disabled={submitting} />
+            </div>
+            <button type="submit" disabled={pristine || submitting}>Submit</button>
+        </form>
+    );
+};
+
+export default reduxForm<Quote, Props>({form: 'quote'})(connect(
+    (state: { form: { quote?: { submitErrors?: Errors } } }) => ({
+        errors: state.form.quote ? state.form.quote.submitErrors : {},
+    })
+)(QuoteForm));
